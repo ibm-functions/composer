14e0d2abdab59cd5a0abcde4870d63dee06cc758
Olivier Tardieu <tardieu@us.ibm.com>
2018-04-20 10:21:17 -0400

    Add 'composer --version'

--- composer.js
+++ composer.js
@@ -437,6 +437,10 @@ function composer() {
             composition = encode(composition)
             return { composition, actions }
         }
+
+        get version() {
+            return version
+        }
     }
 
     return new Composer()
