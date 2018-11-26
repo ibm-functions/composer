2405cf7f3333ef7957c8c8ad5631d1c958160783
Olivier Tardieu <tardieu@users.noreply.github.com>
2018-11-26 15:34:23 -0500

    Skip OpenWhisk build in Travis (#10)

--- travis/setup.sh
+++ travis/setup.sh
@@ -23,26 +23,21 @@ set -e
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ROOTDIR="$SCRIPTDIR/.."
-IMAGE_PREFIX="composer"
 WHISKDIR="$ROOTDIR/openwhisk"
 
-# OpenWhisk stuff
+# Clone OpenWhisk
 cd $ROOTDIR
 git clone --depth=1 https://github.com/apache/incubator-openwhisk.git openwhisk
-cd openwhisk
-./tools/travis/setup.sh
 
+# Install Ansible
+pip install --user ansible==2.5.2
+
+# Configure runtimes
 cp $SCRIPTDIR/runtimes.json $WHISKDIR/ansible/files
-
-# Pull down images
-docker pull openwhisk/controller
-docker tag openwhisk/controller ${IMAGE_PREFIX}/controller
-docker pull openwhisk/invoker
-docker tag openwhisk/invoker ${IMAGE_PREFIX}/invoker
 
 # Deploy OpenWhisk
 cd $WHISKDIR/ansible
-ANSIBLE_CMD="ansible-playbook -i ${WHISKDIR}/ansible/environments/local -e docker_image_prefix=${IMAGE_PREFIX}"
+ANSIBLE_CMD="ansible-playbook -i ${WHISKDIR}/ansible/environments/local -e docker_image_prefix=openwhisk"
 $ANSIBLE_CMD setup.yml
 $ANSIBLE_CMD prereq.yml
 $ANSIBLE_CMD couchdb.yml
@@ -50,13 +45,13 @@ $ANSIBLE_CMD initdb.yml
 $ANSIBLE_CMD wipe.yml
 $ANSIBLE_CMD openwhisk.yml -e cli_installation_mode=remote -e limit_invocations_per_minute=600
 
+# Log configuration
 docker images
 docker ps
-
 curl -s -k https://172.17.0.1 | jq .
 curl -s -k https://172.17.0.1/api/v1 | jq .
 
-# Setup
+# Setup CLI
 WHISK_APIHOST="172.17.0.1"
 WHISK_AUTH=`cat ${WHISKDIR}/ansible/files/auth.guest`
 WHISK_CLI="${WHISKDIR}/bin/wsk -i"
